f4051574dab38d232ce2d424ddb945e4ab6b1e3b
Partha <parthasarthyiitvns@gmail.com>
2021-03-08 16:44:06 +0000

    NLU Instace

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = new express();
+dotenv.config();
 
 app.use(express.static('client'))
 
@@ -26,6 +28,15 @@ app.get("/text/emotion", (req,res) => {
 app.get("/text/sentiment", (req,res) => {
     return res.send("text sentiment for "+req.query.text);
 });
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({version: '2020-08-01', authenticator: new IamAuthenticator({apikey: '{apikey}',}), serviceUrl: '{url}',});
+    return naturalLanguageUnderstanding;
+}
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
